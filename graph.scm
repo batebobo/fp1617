3dc52c4df772628634388bf9cc71b02dbff3b8b3
Boyan Vushkov <bvushkov@gmail.com>
2017-01-29 22:00:05 +0200

    Edit solutions due to recent comments

--- graph.scm
+++ graph.scm
@@ -20,13 +20,13 @@
   (define (make-edges lst)
     (map (lambda (n) (cons (car lst) n)) (cdr lst)))
   (define list-with-edges (map make-edges graph))
-  (foldr append '() list-with-edges))
+  (fold-right append '() list-with-edges))
 
 ;(edges graph) - same thing as above, but using fancy scheme constructs. Keeping it classy
 (define (edges* graph)
   (let* ([make-edges (lambda (lst) (map (lambda (n) (cons (car lst) n)) (cdr lst)))]
          [list-of-edges (map make-edges graph)])
-    (foldr append '() list-of-edges)))
+    (fold-right append '() list-of-edges)))
 
 ;(out-degree vertex graph) - returns the number of vertices, a given vertex "points" to
 (define (out-degree vertex graph)
@@ -34,7 +34,7 @@
 
 ;(in-degree vertex graph) - return the number of vertices that "point" to a given vertex
 (define (in-degree vertex graph)
-  (foldl + 0 (map (lambda (lst) (length (filter (lambda (x) (equal? vertex x)) (cdr lst)))) graph)))
+  (fold-left + 0 (map (lambda (lst) (length (filter (lambda (x) (equal? vertex x)) (cdr lst)))) graph)))
 
 ;(in-degree vertex graph) - same as above but better looking
 (define (in-degree* vertex graph)
@@ -48,7 +48,7 @@
 (define (subgraph? graph1 graph2)
   (let* ([first-edges (edges graph1)]
          [second-edges (edges graph2)]
-         [all? (lambda (p xs) (fold-left (lambda (acc next) (and next acc)) #t (map p xs)))]
+         [all? (lambda (p xs) (fold-left (lambda (acc next) (and acc next)) #t (map p xs)))]
          [subset? (lambda (xs ys) (all? (lambda (x) (member x ys)) xs))])
     (subset? first-edges second-edges)))
 (define graph2 '((1 3 4 5 7 6) (2 1 5) (3 4 2 5) (4 4 1) (5 5)))
